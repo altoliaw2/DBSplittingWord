a7c47a3d9e073bd14da8eb980e103e3986b85f0f
altoliaw <altoliaw@hotmail.com>
2021-06-11 15:35:43 +0800

    Revision

--- main.cpp
+++ main.cpp
@@ -2,34 +2,10 @@
 #include <string>
 
 int fn_IsExisted(std::string* sp_Set, int& i_Size
-                 ,std::string s_Word){
+                 ,std::string s_Word);
 
-	for (int i_Ct = 0; i_Ct < i_Size; i_Ct++){
-        if (sp_Set[i_Ct].compare(s_Word) == 0){
-            return true;
-        }
-	}
-	return false;
-}
-
-bool fn_BreakWord(std::string* sp_Set, int& i_Size, std::string s_Msg){
-	int i_MsgSize = s_Msg.length();
-
-	if (i_MsgSize == 0){
-        return true;
-	}
-
-	for (int i_Ct= 1; i_Ct<= i_MsgSize; i_Ct++){
-		if (
-            fn_IsExisted( sp_Set, i_Size, s_Msg.substr(0, i_Ct) ) &&
-			fn_BreakWord( sp_Set, i_Size, s_Msg.substr(i_Ct, i_MsgSize- i_Ct) )
-        ){
-            return true;
-        }
-
-	}
-	return false;
-}
+bool fn_BreakWord(std::string* sp_Set, int& i_Size,
+                  std::string s_Msg);
 
 int main(){
     int i_Size = -1;
@@ -49,4 +25,35 @@ int main(){
         std::cout<< "TRUE";
     }
 	return 0;
+}
+
+int fn_IsExisted(std::string* sp_Set, int& i_Size
+                 ,std::string s_Word){
+
+	for (int i_Ct = 0; i_Ct < i_Size; i_Ct++){
+        if (sp_Set[i_Ct].compare(s_Word) == 0){
+            return true;
+        }
+	}
+	return false;
+}
+
+bool fn_BreakWord(std::string* sp_Set, int& i_Size,
+                  std::string s_Msg){
+	int i_MsgSize = s_Msg.length();
+
+	if (i_MsgSize == 0){
+        return true;
+	}
+
+	for (int i_Ct= 1; i_Ct<= i_MsgSize; i_Ct++){
+		if (
+            fn_IsExisted( sp_Set, i_Size, s_Msg.substr(0, i_Ct) ) &&
+			fn_BreakWord( sp_Set, i_Size, s_Msg.substr(i_Ct, i_MsgSize- i_Ct) )
+        ){
+            return true;
+        }
+
+	}
+	return false;
 }
